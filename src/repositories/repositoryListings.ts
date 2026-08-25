9895b5c2d8f72644d62246aacf7b021cfbdbdb62
MironDragos <miron.dragos@ceiti.md>
2026-08-25 20:15:58 +0300

    fix: reintroduced floors in duplicating check

--- src/repositories/repositoryListings.ts
+++ src/repositories/repositoryListings.ts
@@ -72,16 +72,18 @@ export async function findPotentialDuplicate(listing: Listing) {
            AND house_number = $2
            AND house_number != ''
            AND rooms = $3 
-           AND m2 BETWEEN $4 - 5 AND $4 + 5
-           AND offer_type = $5 
-           AND price BETWEEN $6 * 0.80 AND $6 * 1.20
+           AND floor = $4
+           AND m2 BETWEEN $5 - 5 AND $5 + 5
+           AND offer_type = $6
+           AND price BETWEEN $7 * 0.80 AND $7 * 1.20
            AND active = true 
-           AND id_extern != $7
+           AND id_extern != $8
          LIMIT 1`,
     [
       listing.street,
       listing.houseNumber,
       listing.rooms,
+      listing.floor,
       listing.m2,
       listing.offer_type,
       listing.price,
